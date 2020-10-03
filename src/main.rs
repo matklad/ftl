b33e36daeaf503b9e73fda79e179bfdc2bf42f09
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-10-03 21:29:28 +0200

    Add pthread API

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn sum_rust() -> u32 {
   COUNTER.with(|it| it.get())
 }
 
-fn with_counter<T>(f: impl FnOnce(&Cell<u32>) -> T) -> T {
+fn with_c_counter<T>(f: impl FnOnce(&Cell<u32>) -> T) -> T {
   extern "C" {
     fn get_thread_local() -> *mut u32;
   }
@@ -28,12 +28,34 @@ fn with_counter<T>(f: impl FnOnce(&Cell<u32>) -> T) -> T {
 
 fn sum_rust_c() -> u32 {
   for step in 0..STEPS {
-    with_counter(|it| {
+    with_c_counter(|it| {
       let inc = step.wrapping_mul(step) ^ step;
       it.set(it.get().wrapping_add(inc))
     })
   }
-  with_counter(|it| it.get())
+  with_c_counter(|it| it.get())
+}
+
+static mut KEY: libc::pthread_key_t = 0;
+
+fn with_pthread_counter<T>(
+  f: impl FnOnce(&Cell<u32>) -> T,
+) -> T {
+  unsafe {
+    let ptr = libc::pthread_getspecific(KEY);
+    let cell: &Cell<u32> = &*ptr.cast();
+    f(cell)
+  }
+}
+
+fn sum_pthread() -> u32 {
+  for step in 0..STEPS {
+    with_pthread_counter(|it| {
+      let inc = step.wrapping_mul(step) ^ step;
+      it.set(it.get().wrapping_add(inc))
+    })
+  }
+  with_pthread_counter(|it| it.get())
 }
 
 fn sum_local() -> u32 {
@@ -75,21 +97,39 @@ fn main() {
       t.elapsed().as_millis()
     );
   }
-  {
-    let t = Instant::now();
-    let r = sum_local();
-    eprintln!(
-      "local variable:  {} {}ms",
-      r,
-      t.elapsed().as_millis()
-    );
-  }
   #[cfg(feature = "unstable")]
   {
     let t = Instant::now();
     let r = sum_rust_unstable();
     eprintln!(
       "#[thread_local]: {} {}ms",
+      r,
+      t.elapsed().as_millis()
+    );
+  }
+  {
+    unsafe {
+      let cell: Box<Cell<u32>> = Box::new(Cell::new(0u32));
+      let cell = Box::into_raw(cell);
+      unsafe extern "C" fn free(ptr: *mut libc::c_void) {
+        let _: Box<Cell<u32>> = Box::from_raw(ptr.cast());
+      }
+      libc::pthread_key_create(&mut KEY as *mut _, Some(free));
+      libc::pthread_setspecific(KEY, cell.cast());
+    }
+    let t = Instant::now();
+    let r = sum_pthread();
+    eprintln!(
+      "pthread:         {} {}ms",
+      r,
+      t.elapsed().as_millis()
+    );
+  }
+  {
+    let t = Instant::now();
+    let r = sum_local();
+    eprintln!(
+      "local variable:  {} {}ms",
       r,
       t.elapsed().as_millis()
     );
